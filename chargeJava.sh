f19543644b1c8b93d60253b62d032f2598093f4a
rbizoi <razvan.bizoi@gmail.com>
2021-11-30 10:54:32 +0100

    Update chargeJava.sh

--- chargeJava.sh
+++ chargeJava.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sftp -P 2424 coursDB@51.91.76.248<<FIN_FICHIER
-put java-8-openjdk-amd64.tar.gz spark
+get spark/java-8-openjdk-amd64.tar.gz
 exit
+FIN_FICHIER
+
+cat << FIN_FICHIER >> .profile
+export JAVA_HOME=~/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH:$HOME/bin
 FIN_FICHIER
